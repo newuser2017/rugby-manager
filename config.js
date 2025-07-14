d6f0c81234fafb689b537fd762722f3eff60ad9e
newuser2017 <luketeeling@gmail.com>
2025-07-14 13:28:41 +0100

    Attempt 2

---
+++ config.js
@@ -0,0 +1,31 @@
+// Configuration for Clontarf Rugby Team Management
+const CONFIG = {
+    // Change this password to whatever you want
+    COACH_PASSWORD: 'clontarf2025',
+    
+    // Your Google Apps Script Web App URL (update after deployment)
+    WEB_APP_URL: 'https://script.google.com/macros/s/YOUR_DEPLOYMENT_ID/exec',
+    
+    // Team settings
+    CLUB_NAME: 'Clontarf Rugby',
+    SQUADS: ['4ths', '5ths'],
+    
+    // Position mappings
+    POSITIONS: {
+        '1': 'Loosehead Prop',
+        '2': 'Hooker', 
+        '3': 'Tighthead Prop',
+        '4': 'Lock',
+        '5': 'Lock',
+        '6': 'Blindside Flanker',
+        '7': 'Openside Flanker',
+        '8': 'Number 8',
+        '9': 'Scrum-half',
+        '10': 'Fly-half',
+        '11': 'Left Wing',
+        '12': 'Inside Centre',
+        '13': 'Outside Centre',
+        '14': 'Right Wing',
+        '15': 'Fullback'
+    }
+};
