3b5bd2d4967f7c6119f6c4880ac35a03e26e1bf7
newuser2017 <luketeeling@gmail.com>
2025-07-18 15:54:07 +0100

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 const CONFIG = {
     // ✅ YOUR EXISTING SETTINGS (UNCHANGED)
     COACH_PASSWORD: 'clontarf2025',
-    WEB_APP_URL: 'https://script.google.com/macros/s/AKfycbw9sSZwEEY-fl1ZnbYP1yNPc2H_kkwIHet4Z6m_VurCi1tyHxFnVl4KZT-KeFE75r75/exec',
+    WEB_APP_URL: 'https://script.google.com/macros/s/AKfycbyh9AJRrFptPi1VlORm7OQp5Q60QWItQ3O0Jjqq227kstEy1sdx-jGj_dDNDNF_sAWi/exec',
     CLUB_NAME: 'Clontarf Rugby',
     SQUADS: ['4ths', '5ths'],
     POSITIONS: {
